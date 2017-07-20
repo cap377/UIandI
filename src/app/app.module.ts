18f6142ff388663aa3a2015079641d7020fc7604
Cherie Parsons <cherie.parsons@siriuscom.com>
2017-07-20 15:44:59 -0500

    added routing and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ForumComponent } from './forum/forum.component';
 import { UserProfilesComponent } from './user-profiles/user-profiles.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ForumComponent,
-    UserProfilesComponent
+    UserProfilesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
