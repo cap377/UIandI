1fb3e335f9803831be79f1478908b3627770a551
Cherie Parsons <cherie.parsons@siriuscom.com>
2017-07-21 11:20:08 -0500

    mobile nav bar and title updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title = 'ACE';
 
+  constructor( private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit() {
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(
+      event => {
+        let currentRoute = this.route.root;
+        while (currentRoute.children[0] !== undefined) {
+          currentRoute = currentRoute.children[0];
+        }
+        this.title = (currentRoute.snapshot.data.name);
+      })
+  }
 
   openSidebar() {
     document.getElementById("main").style.marginLeft = "25%";
