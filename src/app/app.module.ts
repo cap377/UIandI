efa656ffdcecc247f654f34ff6d19b2bc0251e59
Scout Martinelli <scout.martinelli@siriuscom.com>
2017-07-21 13:25:30 -0500

    user profile information updated with sliding information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ForumComponent } from './forum/forum.component';
@@ -15,6 +16,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
