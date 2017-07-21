88c71adce4f5c150db390dba65bf3c850f77942f
William Cortes <will.cortes@siriuscom.com>
2017-07-21 11:21:52 -0500

    deleted uneeded code from app.component.ts and styled forum modal a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,9 @@ export class AppComponent {
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "50%";
     document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
   }
   closeSidebar() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
   }
 }
